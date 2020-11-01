7b8da0941a2f30e18860e338878931e094d07193
Jose Francisco sakuda castro <jsakuda@Joses-MacBook-Pro.local>
2020-11-01 17:08:12 -0500

    heroku3, subida de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactosComponent } from './components/contactos/contactos.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'home', component:HomeComponent},
+  {path: 'servicios', component: ServiciosComponent},
+  {path: 'contacto', component:ContactosComponent},
+  {path: 'productos', component:ProductosComponent},
+  {path: 'quienes-somos', component:QuienesSomosComponent},
+  {path: 'galeria', component:GaleriaComponent},
+  {path:'**', redirectTo:'home'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactosComponent } from './components/contactos/contactos.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactosComponent,
+    FooterComponent,
+    GaleriaComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProductosComponent,
+    QuienesSomosComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule,
